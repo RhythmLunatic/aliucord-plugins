92f0be20a5c2476ca2f2f3a6300c119433334df9
RhythmLunatic <kirbyrulez@att.net>
2021-09-23 15:44:12 -0500

    Second commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,4 @@
 include(":CopyInsteadOfShareImages")
 project(":CopyInsteadOfShareImages").projectDir = File("./CopyInsteadOfShareImages")
 
-include(":CatApi")
-project(":CatApi").projectDir = File("./ExamplePlugins/CatApi")
-
-include(":HelloWorld")
-project(":HelloWorld").projectDir = File("./ExamplePlugins/HelloWorld")
-
-include(":HelloWorldAdvanced")
-project(":HelloWorldAdvanced").projectDir = File("./ExamplePlugins/HelloWorldAdvanced")
-
-//include(":MyFirstPatch")
-//project(":MyFirstPatch").projectDir = File("./ExamplePlugins/MyFirstPatch")
-
 rootProject.name = "AliucordPlugins"
